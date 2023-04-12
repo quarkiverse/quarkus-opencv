ca13e25fe90e768358e5f1e2657ea90a081ec662
Guillaume Smet <guillaume.smet@gmail.com>
2023-04-12 15:40:43 +0200

    Upgrade to OpenCV 4.7

--- integration-tests/src/main/java/io/quarkiverse/opencv/it/OpenCVResource.java
+++ integration-tests/src/main/java/io/quarkiverse/opencv/it/OpenCVResource.java
@@ -16,14 +16,14 @@
 */
 package io.quarkiverse.opencv.it;
 
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 @Path("/opencv")
 @ApplicationScoped
