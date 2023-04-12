5a33f887bf669f70e91bc1bfd526e1ac61460350
Guillaume Smet <guillaume.smet@gmail.com>
2023-04-12 15:29:42 +0200

    Upgrade to Quarkus 3.0.0.Final

--- integration-tests/src/test/java/io/quarkiverse/opencv/it/NativeOpenCVResourceIT.java
+++ integration-tests/src/test/java/io/quarkiverse/opencv/it/NativeOpenCVResourceIT.java
@@ -1,7 +1,7 @@
 package io.quarkiverse.opencv.it;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 public class NativeOpenCVResourceIT extends OpenCVResourceTest {
 }
